9cebc9bf91b5968043e6099646ddd8a62050b7b3
unknown <fmansouri@ce.sharif.edu>
2020-05-11 22:45:36 +0430

    refactoring complete catch

--- src/main/java/codegenerator/CodeGenerator.java
+++ src/main/java/codegenerator/CodeGenerator.java
@@ -192,7 +192,9 @@ public class CodeGenerator {
         try {
             symbolTable.getNextParam(className, methodName);
             Utility.printError("The few argument pass for method");
-        } catch (IndexOutOfBoundsException e) {}
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
             VarType t = VarType.Int;
             switch (symbolTable.getMethodReturnType(className, methodName))
             {
